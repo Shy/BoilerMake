a3635c27fc788953862f8c18b495b4b6e8d1c924
Scott Resnick <Tocs1001@gmail.com>
2014-02-08 09:02:31 -0500

    Initial Commit.

---
+++ Assets/PhysUtil.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysUtil
+{
+    public static void ForceTrack(Rigidbody body, Vector3 position, Quaternion rotation, float maxaccel, float maxangleaccel)
+    {
+
+
+        Vector3 targetvel = (position - body.transform.position) / Time.deltaTime;
+        float targetvelmag = targetvel.magnitude;
+
+        float velmag = body.velocity.magnitude;
+
+        Vector3 diffvel = targetvel - body.velocity;
+
+        if (diffvel.magnitude > maxaccel)
+        {
+            diffvel *= maxaccel / diffvel.magnitude;
+        }
+
+        Vector3 f = (diffvel) * body.mass;
+        body.AddForce(f, ForceMode.Impulse);
+
+
+        Quaternion targetangluarvel = rotation * (Quaternion.Inverse(body.transform.rotation));
+        float targetavelangle; Vector3 targetavelaxis;
+        targetangluarvel.ToAxisAngle(out targetavelaxis, out targetavelangle);
+
+        if (targetavelangle > Mathf.PI * 2 - targetavelangle) //Be careful not to take the long way around the circle.
+        {
+            targetavelaxis *= -1;
+            targetavelangle = 2 * Mathf.PI - targetavelangle;
+        }
+
+        if (targetavelangle == 0)
+            return;
+
+        targetavelangle /= Time.deltaTime;
+
+        Vector3 targetw = targetavelaxis.normalized * targetavelangle;
+
+        Vector3 wf = (targetw - body.angularVelocity);
+
+        if (wf.magnitude > maxangleaccel)
+        {
+            wf *= maxangleaccel / wf.magnitude;
+        }
+
+        Quaternion qi = body.transform.rotation * body.inertiaTensorRotation;
+        Vector3 t = qi * Vector3.Scale(body.inertiaTensor, (Quaternion.Inverse(qi) * wf));
+
+
+        body.AddTorque(t, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Sword/Sword.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour 
+{
+    public Transform Player;
+
+    Vector3 ChestPosition;
+    Vector3 ArmLengthPosition;
+
+
+    Vector3 TargetPosition;
+    Quaternion TargetRotation;
+
+    float Scale;
+
+    enum SyncMode
+    {
+        Starting,
+        Chest,
+        Waiting,
+        ArmLength,
+        Done
+    }
+    SyncMode Mode = SyncMode.Starting;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rigidbody.maxAngularVelocity = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        SixenseInput.Controller controller = SixenseInput.GetController(SixenseHands.LEFT);
+        if (controller == null || !controller.Enabled)
+            return;
+
+        if (Mode == SyncMode.Starting)
+        {
+            if (controller.Trigger <= 0)
+            {
+                Mode = SyncMode.Chest;
+            }
+            return;
+        }
+        if (Mode == SyncMode.Chest)
+        {
+            if (controller.Trigger > 0.5)
+            {
+                ChestPosition = controller.Position;
+                Mode = SyncMode.Waiting;
+            }
+            return;
+        }
+        else if (Mode == SyncMode.Waiting)
+        {
+            if (controller.Trigger <= 0)
+            {
+                Mode = SyncMode.ArmLength;
+            }
+            return;
+        }
+        else if (Mode == SyncMode.ArmLength)
+        {
+            if (controller.Trigger > 0.5)
+            {
+                ArmLengthPosition = controller.Position;
+                Mode = SyncMode.Done;
+            }
+            return;
+        }
+
+        Scale = (ArmLengthPosition - ChestPosition).magnitude;
+
+
+        TargetPosition = Player.position + new Vector3(0, 1.54f, 0) + Player.rotation * ((controller.Position - ChestPosition) / Scale); //* (0.8382f); //Magical arm constant
+        TargetRotation = Player.rotation * controller.Rotation;
+
+       
+	}
+
+    void FixedUpdate()
+    {
+        PhysUtil.ForceTrack(rigidbody, TargetPosition, TargetRotation, 1000, 1000);
+    }
+
+    void OnGUI()
+    {
+        if (Mode == SyncMode.Chest)
+        {
+            uint boxWidth = 300;
+            uint boxHeight = 24;
+            string boxText = "Place Controller At Chest and Pull Trigger";
+            GUI.Box(new Rect(((Screen.width / 2) - (boxWidth / 2)), ((Screen.height / 2) - (boxHeight / 2)) -48, boxWidth, boxHeight), boxText);
+        }
+        if (Mode == SyncMode.ArmLength)
+        {
+            uint boxWidth = 300;
+            uint boxHeight = 24;
+            string boxText = "Place Controller At Arms Length and Pull Trigger";
+            GUI.Box(new Rect(((Screen.width / 2) - (boxWidth / 2)), ((Screen.height / 2) - (boxHeight / 2)) - 48, boxWidth, boxHeight), boxText);
+        }
+    }
+}
