00436c92423fcb767c5304c4ed392e939e25e9c3
Scott Resnick <Tocs1001@gmail.com>
2014-02-09 07:26:31 -0500

    WIP Reattachment

--- Assets/Player/Frankenstein.cs
+++ Assets/Player/Frankenstein.cs
@@ -5,14 +5,46 @@ public class Frankenstein : MonoBehaviour
 {
     Vector3 StartPos;
     Quaternion StartRot;
-    Transform Parent;
+    public Transform Body;
+    Player player;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        StartPos = transform.localPosition;
+        StartRot = transform.localRotation;
+        Body = transform.root;
+        player = Body.GetComponent<Player>();
 	}
-	
+
+    void ReAttach()
+    {
+        Transform Container = Body.GetChild(0);
+
+        Transform OVRcam = transform.FindChild("OVRCameraController");
+
+        OVRcam.parent = Container;
+
+        OVRcam.GetComponent<OVRCameraController>().FollowOrientation = Body;
+
+        OVRcam.localPosition = new Vector3(0,1.74f,0);
+        OVRcam.localRotation = new Quaternion();
+
+        transform.parent = OVRcam.GetChild(0);
+        transform.localPosition = StartPos;
+        transform.localRotation = StartRot;
+
+        Destroy(rigidbody);
+        
+    }
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        if (Network.isServer)
+        {
+            if (player.Type == Player.PlayerType.Host && Input.GetKeyDown(KeyCode.D))
+            {
+            }
+        }
 	}
 }
